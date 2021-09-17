7c4fb346f60380337379551da5e4abb0a796df7c
insanedude59 <80731924+insanedude59@users.noreply.github.com>
2021-09-16 21:49:27 -0400

    Update opensource.lua

--- opensource.lua
+++ opensource.lua
@@ -1,4 +1,5 @@
-while wait(0.001) do
+getgenv().autofarm = true
+while getgenv().autofarm == true do wait()
    for i,v in pairs(game.Workspace:GetDescendants()) do
     if v.Name == "0" and v.Parent.Name == "MAP_1_0" then
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(v.Position)
