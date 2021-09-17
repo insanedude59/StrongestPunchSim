ba24900712924e95709801a17b5e0765fe0b0797
insanedude59 <80731924+insanedude59@users.noreply.github.com>
2021-09-16 21:50:02 -0400

    Update opensource.lua

--- opensource.lua
+++ opensource.lua
@@ -1,4 +1,3 @@
-getgenv().autofarm = true
 while getgenv().autofarm == true do wait()
    for i,v in pairs(game.Workspace:GetDescendants()) do
     if v.Name == "0" and v.Parent.Name == "MAP_1_0" then
