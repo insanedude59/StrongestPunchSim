535baa224dd0e10ed6c7a3c8044a84a778c0e3fe
insanedude59 <80731924+insanedude59@users.noreply.github.com>
2021-09-16 21:46:17 -0400

    Add files via upload

---
+++ opensource.lua
@@ -0,0 +1,5 @@
+for i,v in pairs(game.Workspace:GetDescendants()) do
+    if v.Name == "0" and v.Parent.Name == "MAP_1_0" then
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(v.Position)
+        end
+    end
